ac0ee04cff14aebd8c4682d6f242e8de6aab94b4
Jordan <kellyje@outlook.com>
2013-03-23 21:01:17 -0400

    Updated chrome extension to only inject javascript file. Loading of styles will be done within the script.

--- extensions/chrome/chromeContent.js
+++ extensions/chrome/chromeContent.js
@@ -1,10 +1,3 @@
 //Inject javascript
 var URL = "https://localhost/meteor/public/github.js";
-document.body.appendChild(document.createElement("script")).src=URL;
-
-//Inject css
-var link = document.createElement("link");
-link.href = "https://localhost/meteor/public/codebounty.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild(link);
+document.body.appendChild(document.createElement("script")).src = URL;
