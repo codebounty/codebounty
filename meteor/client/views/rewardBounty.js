43088331ce7633a3b7ebb54e357825ca0e4284e6
Jordan <kellyje@outlook.com>
2013-03-24 20:38:52 -0400

    Added jquery-ui. Fixed some iframe styling. Added basic linking logic to reward slider, input, and percentage.

--- meteor/client/views/rewardBounty.js
+++ meteor/client/views/rewardBounty.js
@@ -11,6 +11,42 @@ Template.rewardBountyView.rendered = function () {
 
         Session.set("contributors", result);
     });
+
+    var bountyTotal = 200;
+    var currentVal = 45;
+    $(".contributorRow").each(function (index) {
+        var thisRow = this;
+        $(this).find(".rewardSlider").slider({
+            animate: "fast",
+            value: currentVal,
+            max: bountyTotal,
+            slide: function (event, ui) {
+                var max = $(this).slider("option", "max");
+                $(thisRow).find(".rewardInput").val(ui.value);
+                $(thisRow).find(".rewardPercent").val(((ui.value / max) * 100).toFixed(2));
+            }
+        });
+        $(this).find(".rewardInput")
+            .val(currentVal)
+            .change(function () {
+                var val = parseInt($(this).val());
+                var max = $(thisRow).find(".rewardSlider").slider("option", "max");
+                //TODO: Input validation
+                $(".rewardSlider").slider('value', val);
+                $(thisRow).find(".rewardPercent").val(((val / max) * 100).toFixed(2));
+            }
+        );
+        $(this).find(".rewardPercent")
+            .val(((currentVal / $(thisRow).find(".rewardSlider").slider("option", "max")) * 100).toFixed(2))
+            .change(function () {
+                var max = $(thisRow).find(".rewardSlider").slider("option", "max");
+                var val = parseFloat(($(this).val() / 100) * max);
+                //TODO: Input validation
+                $(".rewardSlider").slider('value', val);
+                $(thisRow).find(".rewardInput").val(val);
+            }
+        );
+    });
 };
 
 Template.rewardBountyView.events({
