998994979d004eaedcab24a74b7ef9dbf467baa0
Jon Perl <perl.jonathan@gmail.com>
2013-04-11 00:47:35 -0400

    setup cache to handle multiple pages / update each if their etag changes. just need to setup crawler to crawl continually if there are more pages, then should be done.

--- meteor/server/lib/github.js
+++ meteor/server/lib/github.js
@@ -40,6 +40,152 @@ CB.GitHub = (function () {
     };
 
     /**
+     * Run a request then trigger the callback
+     * @param name the request name
+     * @param data the request data
+     * @param [etag] will run a conditional request if passed
+     * @param [page] the page to get. defaults to 1
+     * @param callback (error, result)
+     */
+    GitHub.prototype._runRequest = function (name, data, etag, page, callback) {
+        //clone data to use for request options
+        var requestOptions = JSON.parse(JSON.stringify(data));
+        requestOptions.page = page || 1;
+        //max allowed http://developer.github.com/v3/#pagination
+        requestOptions.per_page = 100;
+
+        //if there is a cached response perform a conditional request
+        var headers = {};
+        if (etag) {
+            headers["If-None-Match"] = etag;
+        }
+
+        var requestFunction;
+
+        if (name === "Issues.getEvents") {
+            requestFunction = this._client.issues.getEvents;
+        } else {
+            throw "Not a known request: " + request;
+        }
+
+        //run the request
+        requestFunction(requestOptions, callback, headers);
+    };
+
+    /**
+     * finds and updates, or creates an initial cached response
+     * NOTE: this does not update the DB, that is done later when it is crawled to the end
+     * @param request
+     * @param data
+     * @param callback (error, result) returns the cachedResponse
+     */
+    GitHub.prototype._updateCachedResponse = function (request, data, callback) {
+        var that = this;
+        Fiber(function () {
+            var cachedResponse = Responses.findOne({request: request, data: data});
+            //if there is not a cached response yet
+            //load the first page and set it on a new cached response
+            if (!cachedResponse) {
+                cachedResponse = {
+                    request: request,
+                    data: data,
+                    pages: []
+                };
+
+                that._runRequest(request, data, null, 1, function (err, res) {
+                    var pageResponse = {
+                        meta: res.meta
+                    };
+                    delete res.meta;
+                    pageResponse.data = res;
+
+                    cachedResponse.pages.push(pageResponse);
+
+                    if (err)
+                        callback(err);
+                    else
+                        callback(null, cachedResponse);
+                });
+                return;
+            }
+
+            //if there is a cached response do a conditional request for each cached page
+            async.each(cachedResponse.pages, function (pageResponse, pageChecked) {
+                var pageIndex = _.indexOf(cachedResponse.pages, pageResponse);
+                that._runRequest(request, data, pageResponse.meta.etag, pageIndex, function (err, res) {
+                    if (!err) {
+                        //if the page changed, update it
+                        if (res.meta.etag !== pageResponse.meta.etag) {
+                            pageResponse.meta = res.meta;
+                            delete res.meta;
+                            pageResponse.data = res;
+                        }
+                    }
+
+                    pageChecked(err);
+                });
+            }, function (err) {
+                if (err)
+                    callback(err);
+                else
+                    callback(null, cachedResponse);
+            });
+        }).run();
+    };
+
+    /**
+     * Perform a conditional request for each page of a request
+     * then cache and return the result
+     * @param request the request name
+     * @param data the request data (no config options like page #)
+     *             used as a lookup in the cache for existing responses
+     * @param callback (error, result) called after completed. passed the result pages concatenated
+     */
+    GitHub.prototype.ConditionalCrawlAndCache = function (request, data, callback) {
+        var that = this;
+        async.waterfall([
+            //update the cache
+            function (cb) {
+                that._updateCachedResponse(request, data, cb);
+            },
+
+            //then crawl until the last page
+            function (cachedResponse, cb) {
+                //TODO
+                cb(null, cachedResponse);
+            }],
+
+            //then store the result and return it
+            function (err, cachedResponse) {
+                if (err) {
+                    console.log("ERROR: ConditionalCrawlAndCache", err, request, data);
+                    return;
+                }
+
+                Fiber(function () {
+                    //update cached response if it already exists
+                    if (cachedResponse._id) {
+                        Responses.update(cachedResponse._id, {$set: {pages: cachedResponse.pages}});
+                    }
+                    //otherwise insert it
+                    else {
+                        Responses.insert(cachedResponse);
+                    }
+                }).run();
+
+                //merge the pages before returning them
+
+                var pageData = _.map(cachedResponse.pages, function (page) {
+                    return page.data;
+                });
+
+                var merged = _.flatten(pageData);
+                callback(null, merged);
+            }
+        );
+    };
+
+    /**
      * Loads the issue events with a conditional request
      * TODO paging go through each page
      * @param repo {user: "jperl", name: "codebounty"}
@@ -47,58 +193,11 @@ CB.GitHub = (function () {
      * @param {function} callback (error, result)
      */
     GitHub.prototype.GetIssueEvents = function (repo, issue, callback) {
-        var responseSelector = {
-            request: "Issues.getEvents",
+        this.ConditionalCrawlAndCache("Issues.getEvents", {
             user: repo.user,
             repo: repo.name,
             number: issue
-        };
-
-        var cachedResponse = Responses.findOne(responseSelector);
-
-        //update the cached response then return it
-        var updateCacheCallback = function (error, res) {
-            if (error || !res) {
-                //TODO error logging / handling
-                return;
-            }
-
-            var etag = null;
-            if (res)
-                etag = res.meta.etag;
-
-            //cache the result if the etag changed or if there is no cachedResponse
-            if (!cachedResponse || etag !== cachedResponse.meta.etag) {
-                Fiber(function () {
-                    if (cachedResponse) {
-                        cachedResponse.data = res;
-                        Responses.update(cachedResponse._id, {$set: {meta: res.meta, data: res}});
-                    } else {
-                        cachedResponse = responseSelector;
-                        cachedResponse.meta = res.meta;
-                        cachedResponse.data = res;
-                        Responses.insert(cachedResponse);
-                    }
-                }).run();
-            }
-            //return it
-            callback(null, cachedResponse.data);
-        };
-
-        //if there is a cached response perform a conditional request
-        var headers = {};
-        if (cachedResponse) {
-            headers["If-None-Match"] = cachedResponse.meta.etag;
-        }
-
-        this._client.issues.getEvents({
-                user: repo.user,
-                repo: repo.name,
-                number: issue
-            },
-            updateCacheCallback,
-            headers
-        );
+        }, callback);
     };
 
     GitHub.prototype.GetCommit = function (repo, sha, callback) {
