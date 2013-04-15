c691b72823aa55e89b90c47a3fd2acd973648074
Jordan <kellyje@outlook.com>
2013-04-14 22:24:19 -0400

    Fixed slider. Rounded/truncated values are disabled until bugs are found. Added jquery ui touch punch to solve touch issues. Started working on checkboxes.

--- meteor/client/views/rewardBounty.js
+++ meteor/client/views/rewardBounty.js
@@ -34,84 +34,111 @@ Template.rewardBountyView.minimum = CB.Payout.Minimum;
 Template.rewardBountyView.rendered = function () {
     var contributors = getContributors();
     var total = getTotalBounty();
-    var minimum = CB.Payout.Minimum();
+    var minimum = 0;//CB.Payout.Minimum();
     var numberContributors = contributors.length;
-
     //initialize with an equal split
-    var equalSplit = CB.Tools.Truncate(total / numberContributors, 2);
+    var equalSplit = total / numberContributors; //CB.Tools.Truncate(total / numberContributors, 2);
 
-    var _setHandled;
-    /**
-     * Change the current row's values to this amount. Adjust it if is is < minimum or > total
-     * TODO adjust the other contributor's rewards equally by removing / adding the difference to the total
-     * @param row
-     * @param amount
-     */
-    var setAmount = function (row, amount) {
-        //prevent infinite loop
-        if (_setHandled)
-            return;
-        _setHandled = true;
-
-        var row = $(row);
-
+    //TODO Also check for invalid inputs.
+    //Sets a hard limit on max/min values for amount.
+    var parseAmount = function (amount) {
+        if (!amount) return 0;
         if (amount > total)
             amount = total;
         else if (amount < minimum)
             amount = minimum;
-
-        //truncate decimal past 2 for the amount
-        amount = CB.Tools.Truncate(amount, 2);
-        //for the percent truncate all decimals
-        var percent = (amount / total) * 100;
-        percent = CB.Tools.Truncate(percent, 0);
-
-        row.find(".rewardInput").val(amount);
-        row.find(".rewardPercent").val(percent);
-        row.find(".rewardSlider").slider("value", amount);
-
-        _setHandled = false;
+        return amount;
     };
 
-    $(".contributorRow").each(function (index) {
-        var thisRow = this;
-        $(thisRow).find(".rewardSlider").slider({
+    var setAmount = function (row, amount) {
+        amount = parseAmount(amount);
+        //Move the current amount to previousAmount
+        row.data("previousAmount", row.data("currentAmount"));
+        //Set the currentAmount.
+        row.data("currentAmount", amount);//CB.Tools.Truncate(amount, 2));
+
+        var percent = (amount / total) * 100;
+        row.find(".rewardSlider").slider("value", percent);//CB.Tools.Truncate(percent, 2));
+        row.find(".rewardInput").val(amount);//CB.Tools.Truncate(amount, 2));
+        row.find(".rewardPercent").val(percent);//CB.Tools.Truncate(percent, 2));
+    };
+
+    var updateOtherSliders = function (rowToExclude) {
+        var currentAmount = rowToExclude.data("currentAmount");
+        var previousAmount = rowToExclude.data("previousAmount");
+
+        //Update all rows aside from the one being modified directly.
+        $(".contributorRow").not(rowToExclude).each(function (index, row) {
+            row = $(row);
+
+            var changedAmount;
+            if (previousAmount !== total) {
+                var delta = (total - currentAmount) / (total - previousAmount);
+                changedAmount = delta * row.data("currentAmount");
+            } else {
+                //When max reached.
+                changedAmount = (total - currentAmount) / (numberContributors - 1)
+            }
+
+            setAmount(row, changedAmount);
+        });
+    };
+
+    var toggleRow = function (row) {
+        row.toggleClass("enabled");
+        var disabled = !row.hasClass("enabled");
+        row.find(".rewardSlider").slider("option", "disabled", disabled);
+        row.find(".rewardInput").prop("disabled", disabled);
+        row.find(".rewardPercent").prop("disabled", disabled);
+        //TODO Update row amounts
+    };
+
+    $(".contributorRow").each(function (index, row) {
+        row = $(row);
+        //Slider slide listener
+        //Note: Chrome bug steals drag event from slider.
+        row.find(".shouldPay").click(function (event) {
+            var checkBox = $(event.toElement);
+            var row = checkBox.parent(".contributorRow");
+            toggleRow(row);
+        });
+
+        row.find(".rewardSlider").slider({
             animate: "fast",
-            max: total,
+            min: 0,
+            max: 100,
+            disabled: true,
             slide: function (event, ui) {
-                var amount = ui.value;
-                setAmount(thisRow, amount);
+                var amount = (ui.value / 100) * total;
+                setAmount(row, amount);
+                updateOtherSliders(row);
             }
         });
-        $(thisRow).find(".rewardInput")
+
+        //Input change listener
+        row.find(".rewardInput")
             .change(function () {
-                var amount = parseInt($(this).val());
-                setAmount(thisRow, amount);
-            }
-        );
-        $(thisRow).find(".rewardPercent")
-            .change(function () {
-                var amount = parseFloat(($(this).val() / 100) * total);
-                setAmount(thisRow, amount);
+                var amount = parseFloat($(this).val());
+                setAmount(row, amount);
+                updateOtherSliders(row);
             }
         );
 
-        //set the row initially to an equal split
-        setAmount(thisRow, equalSplit);
+        //Percent input change listener
+        row.find(".rewardPercent")
+            .change(function () {
+                var amount = (parseFloat($(this).val()) / 100) * total;
+                setAmount(row, amount);
+                updateOtherSliders(row);
+            }
+        );
+
+        //Set the row initially to an equal split
+        setAmount(row, equalSplit);
     });
 };
 
 Template.rewardBountyView.events({
-    "click .shouldPay": function (event) {
-        var checkBox = $(event.toElement);
-        var rewardGroup = checkBox.siblings(".rewardInputGroup");
-
-        if (checkBox.is(":checked"))
-            rewardGroup.unblock();
-        else
-            rewardGroup.block({ message: null, overlayCSS: {cursor: 'default' }});
-    },
-
     "click #closeButton": function () {
         Messenger.send({event: "closeOverlay"});
     },
