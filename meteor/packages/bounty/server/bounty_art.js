9e52633ce372acd6b51eeb4e06128206c9d6ecaf
Jordan <kellyje@outlook.com>
2013-04-29 04:03:40 -0400

    #21 Setup basic positioning and styling. Need art assets and fonts to finish up.

--- meteor/packages/bounty/server/bounty_art.js
+++ meteor/packages/bounty/server/bounty_art.js
@@ -14,49 +14,63 @@ Bounty.statusImage = function (bountyId, callback) {
     var status;
     if (bounty.reward) {
         if (bounty.reward.paid) {
-            status = "Paid";
+            status = "paid";
         } else if (bounty.reward.planned) {
-            status = "Planned " + bounty.reward.planned;
+            status = "planned " + bounty.reward.planned;
         }
     } else if (bounty.approved) {
-        status = "Open";
+        status = "open";
     } else {
-        status = "Error?";
+        status = "error?";
     }
 
     var currencySymbol = "$";
+    var leftOffset = 442;
+    var width = 1146;
+    var height = 592;
+    var backgroundColor = "#E1C39B";
+    var textColor = "#333333";
 
     //TODO make this pretty
-    var canvas = new Canvas(1146, 592),
+    var canvas = new Canvas(width, height),
         ctx = canvas.getContext("2d");
 
     //load bounty example example
     fs.readFile(basepath + "assets/bountyExample.png", function (err, src) {
         if (err) throw err;
-        var img = new Image;
-        img.src = src;
 
-        ctx.drawImage(img, 0, 0, img.width, img.height);
+        //Draw background
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(0, 0, width, height);
 
-        var leftOffset = 442;
+        //Reset color
+        ctx.fillStyle = textColor;
 
+        //Draw img asset
+//        var img = new Image;
+//        img.src = src;
+//        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        //Draw Bounty status
         ctx.font = "60px Arial bold";
         var statusHeader = "Bounty now " + status;
         ctx.fillText(statusHeader, leftOffset, 130);
 
+        //Draw bounty amount and expiration
         ctx.font = "42px Arial bold";
         var bountyAmount = "The bounty is posted for " + currencySymbol + bounty.amount;
         ctx.fillText(bountyAmount, leftOffset, 213);
         var bountyExpiration = "Expires: " + "May 20th, 2013 at 12am";
         ctx.fillText(bountyExpiration, leftOffset, 272);
 
+        //Draw codebounty plug and link
+        ctx.textAlign = "right";
         ctx.font = "34px Arial bold";
         var sitePlug = "Get Code Bounty now"
-        ctx.fillText(sitePlug, leftOffset+300, 505);
-
+        ctx.fillText(sitePlug, width-56, 505);
         ctx.font = "30px Arial";
         var siteLink = "codebounty.co"
-        ctx.fillText(siteLink, leftOffset+445, 540);
+        ctx.fillText(siteLink, width-56, 540);
 
         callback(canvas);
     });
