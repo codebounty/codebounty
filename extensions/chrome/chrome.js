386477328412c11964162c1ebba1cb43b0712be8
Kaiwen Xu <kevin@kevxu.net>
2013-05-10 20:47:47 -0400

    Programmatically inject content scripts, fixes #38.
    Now changes whether the url follows the pattern "https://github.com
    /*/*/issues/[decimal number]*", if it is, injects the script when page
    loading.

--- extensions/chrome/chrome.js
+++ extensions/chrome/chrome.js
@@ -3,11 +3,26 @@ chrome.contentSettings = chrome.contentSettings || {};
 
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-    // If the letter 'g' is found in the tab's URL...
-    if (tab.url.indexOf("github.com/jperl/codebounty/issues/") > -1) {
+    // Check whether we are at the valid issue page
+    var validUrlPattern = /https:\/\/github.com\/[^\/ \n\r]+\/[^\/ \n\r]+\/issues\/\d.*/i;
+
+    if (validUrlPattern.test(tab.url)) {
         // ... show the page action.
         chrome.pageAction.show(tabId);
-//        chrome.tabs.insertCSS(tabId, {file: "https://localhost/meteor/public/codebounty.css"});
+        // chrome.tabs.insertCSS(tabId, {file: "https://localhost/meteor/public/codebounty.css"});
+        
+        // Inject content scripts
+        if (changeInfo.status === "loading") {
+            chrome.tabs.executeScript(tabId, {
+                "file": "jquery-1.9.1.min.js",
+                "runAt": "document_end"
+            });
+
+            chrome.tabs.executeScript(tabId, {
+                "file": "chromeContent.js",
+                "runAt": "document_end"
+            });
+        }
     }
 }
 
