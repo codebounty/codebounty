d587663382084d52e7fd8b090d300d0d40e19c2e
Kaiwen Xu <kevin@kevxu.net>
2013-05-11 22:25:39 -0400

    Bounty status image starts working #21.
    Fonts still needs tweaking.

--- meteor/packages/bounty/server/bounty_art.js
+++ meteor/packages/bounty/server/bounty_art.js
@@ -1,5 +1,5 @@
 var fs = Npm.require("fs"), path = Npm.require("path"),
-    basepath = path.resolve(".") + "/packages/bounty/";
+    basepath = path.resolve(".") + "/packages/bounty";
 
 /**
  * generates the bounty status image
@@ -7,69 +7,154 @@ var fs = Npm.require("fs"), path = Npm.require("path"),
  * @param callback passed a {Canvas}
  */
 Bounty.statusImage = function (bountyId, callback) {
-    var bounty = Bounties.findOne(bountyId);
+    var Font = Canvas.Font;
 
-    var status;
-    if (bounty.reward) {
-        if (bounty.reward.paid) {
-            status = "paid";
-        } else if (bounty.reward.planned) {
-            status = "planned " + bounty.reward.planned;
-        }
-    } else if (bounty.approved) {
-        status = "open";
-    } else {
-        status = "error?";
+    // var bounty = Bounties.findOne(bountyId);
+    
+    // TODO: debug purpose, remove it later
+    var bounty = {
+        "status": "reopened",
+        "amount": "10",
+        "cashLevel": 3,
+        "user": "John Doe"
+    }
+    var status = bounty.status;
+
+    // var status;
+    // if (bounty.reward) {
+    //     if (bounty.reward.paid) {
+    //         status = "paid";
+    //     } else if (bounty.reward.planned) {
+    //         status = "planned " + bounty.reward.planned;
+    //     }
+    // } else if (bounty.approved) {
+    //     status = "open";
+    // } else {
+    //     status = "error?";
+    // }
+    
+    var assetFile = function (name) {
+        return path.join(basepath, "/assets/", name);
     }
 
-    var currencySymbol = "$";
-    var leftOffset = 442;
-    var width = 1146;
-    var height = 592;
-    var backgroundColor = "#E1C39B";
-    var textColor = "#333333";
 
-    //TODO make this pretty
+    var currencySymbol = "$";
+    var leftOffset = 271;
+    var rightOffset = 42;
+    var width = 715;
+    var height = 370;
+    var backgroundColor = "#E0C39D";
+    var textColor = "#484640";
+
+    // TODO make this pretty
     var canvas = new Canvas(width, height),
         ctx = canvas.getContext("2d");
 
-    //load bounty example example
-    fs.readFile(basepath + "assets/bountyExample.png", function (err, src) {
-        if (err) throw err;
+    // Setup fonts     
+    var statusHeaderFontName = "Woodshop";
+    var statusHeaderFontFile = "Woodshop-Regular.otf";
+    var statusHeaderFontColor = "#484640";
+    var statusHeaderFontSize = "39px";
+    var statusHeaderFontFace = "normal";
+    var statusHeaderFont = new Font(statusHeaderFontName, assetFile(statusHeaderFontFile));
+    ctx.addFont(statusHeaderFont);
 
-        //Draw background
-        ctx.fillStyle = backgroundColor;
-        ctx.fillRect(0, 0, width, height);
+    var statusContentFontName = '"Futura LT Heavy"';
+    var statusContentFontFile = "Futura-LT-Heavy.otf";
+    var statusContentFontColor = "#78665A";
+    var statusContentFontSize = "30.24px";
+    var statusContentFontFace = "normal";
+    var statusContentFont = new Font(statusContentFontName, assetFile(statusContentFontFile));
+    ctx.addFont(statusContentFont);
 
-        //Reset color
-        ctx.fillStyle = textColor;
+    var statusFooterFontName = '"Futura LT"';
+    var statusFooterFontFile = "Futura-LT.ttf";
+    var statusFooterFontColor = "#484640";
+    var statusFooterFontSize = "30.24px";
+    var statusFooterSmallerFontSize = "20px";
+    var statusFooterFontFace = "normal";
+    var statusFooterFont = new Font(statusFooterFontName, assetFile(statusFooterFontFile));
+    ctx.addFont(statusFooterFont);
 
-        //Draw img asset
-//        var img = new Image;
-//        img.src = src;
-//        ctx.drawImage(img, 0, 0, img.width, img.height);
+     // Draw background
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height);
 
-        //Draw Bounty status
-        ctx.font = "bold 60px Western";
-        var statusHeader = "BOUNTY NOW " + status.toUpperCase() + "!";
-        ctx.fillText(statusHeader, leftOffset, 130);
+    // Reset color
+    ctx.fillStyle = textColor;
 
-        //Draw bounty amount and expiration
-        ctx.font = "bold 42px Arial";
-        var bountyAmount = "The bounty is posted for " + currencySymbol + bounty.amount;
-        ctx.fillText(bountyAmount, leftOffset, 213);
-        var bountyExpiration = "Expires: " + "May 20th, 2013 at 12am";
-        ctx.fillText(bountyExpiration, leftOffset, 272);
+    // Draw Bounty status
+    // TODO: exclamation mark is not included in font Woodshop, so in the
+    // original design, it is replaced by using Myriad Pro.
+    ctx.font = statusHeaderFontFace + " " + statusHeaderFontSize + " " + statusHeaderFontName;
+    ctx.fillStyle = statusHeaderFontColor;
+    var statusHeader;
+    if (status == "open")
+        statusHeader = "BOUNTY NOW OPEN!";
+    else if (status == "closed")
+        statusHeader = "BOUNTY CLOSED";
+    else if (status == "reopened")
+        statusHeader = "BOUNTY REOPENED";
+    else
+        throw "Unknown bounty status";
+    ctx.fillText(statusHeader, leftOffset, 80);
 
-        //Draw codebounty plug and link
-        ctx.textAlign = "right";
-        ctx.font = "34px Arial";
-        var sitePlug = "Get Code Bounty now"
-        ctx.fillText(sitePlug, width-56, 505);
-        ctx.font = "30px Arial";
-        var siteLink = "codebounty.co"
-        ctx.fillText(siteLink, width-56, 540);
+    // Draw bounty amount and expiration
+    ctx.font = statusContentFontFace + " " + statusContentFontSize + " " + statusContentFontName;
+    ctx.fillStyle = statusContentFontColor;
+    var bountyAmount = "The bounty is posted for " + currencySymbol + bounty.amount;
+    ctx.fillText(bountyAmount, leftOffset, 133);
+    var bountyExpiration = "Expires: " + "May 20th, 2013 at 12am";
+    ctx.fillText(bountyExpiration, leftOffset, 170);
 
-        callback(canvas);
-    });
+    //Draw codebounty plug and link
+    ctx.textAlign = "right";
+    ctx.fillStyle = statusFooterFontColor;
+    ctx.font = statusFooterFontFace + " " + statusFooterFontSize + " " + statusFooterFontName;
+    var posterUser = "Posted by " + bounty.user;
+    ctx.fillText(posterUser, width - rightOffset, 305);
+    ctx.font = statusFooterFontFace + " " + statusFooterSmallerFontSize + " " + statusFooterFontName;
+    var siteLink = "codebounty.co"
+    ctx.fillText(siteLink, width - rightOffset, 336);
+
+    // Draw bounty status image
+    var bountyStatusImageFile;
+    if (status == "open")
+        bountyStatusImageFile = "banner-bounty-open.png";
+    else if (status == "closed")
+        bountyStatusImageFile = "banner-bounty-closed.png";
+    else if (status == "reopened")
+        bountyStatusImageFile = "banner-bounty-reopened.png";
+    else
+        throw "Unknown bounty status";
+    var bountyStatusImage = new Image;
+    bountyStatusImage.src = fs.readFileSync(assetFile(bountyStatusImageFile));
+    ctx.drawImage(bountyStatusImage, 0, 0, bountyStatusImage.width, bountyStatusImage.height);
+
+    // Draw bounty cash image
+    var bountyCashImageFile;
+    switch (bounty.cashLevel) {
+        case 1:
+            bountyCashImageFile = "cash-coins.png";
+            break;
+        case 2:
+            bountyCashImageFile = "cash-money-bag.png";
+            break;
+        case 3:
+            bountyCashImageFile = "cash-many-money-bags.png";
+            break;
+        case 4:
+            bountyCashImageFile = "cash-bars.png";
+            break;
+        case 5:
+            bountyCashImageFile = "cash-jackpot.png";
+            break;
+        default:
+            throw "Unknown cash level";
+    }
+    var bountyCashImage = new Image;
+    bountyCashImage.src = fs.readFileSync(assetFile(bountyCashImageFile));
+    ctx.drawImage(bountyCashImage, 171, 249, bountyCashImage.width, bountyCashImage.height);
+
+    callback(canvas);
 };
