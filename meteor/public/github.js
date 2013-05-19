be16fc3c9e8810909c20cdd949b43c053906c726
Ryan Casey <ryepdx@gmail.com>
2013-05-19 07:58:21 -0700

    Realized that the interface I'd implemented was not the one we'd discussed. Implemented that one. Now visiting an issue page pulls an address from the pool of available addresses and creates a Blockchain.info proxy address that triggers a callback to the callbackURI in settings.js. Still need to handle the case where there are no available addresses and need to handle displaying mixed USD and BTC bounties on issue pages.

--- meteor/public/github.js
+++ meteor/public/github.js
@@ -1,5 +1,24 @@
 //the injected github UI
 (function (undefined) {
+    var btcSymbol = "\
+        <svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'\
+             viewBox='0 0 273.6 360' enable-background='new 0 0 273.6 360' xml:space='preserve'\
+             class='btcSymbol'>\
+        <g>\
+            <g>\
+                <path fill='#FFF' d='M217.021,167.042c18.631-9.483,30.288-26.184,27.565-54.007c-3.667-38.023-36.526-50.773-78.006-54.404l-0.008-52.741\
+                    h-32.139l-0.009,51.354c-8.456,0-17.076,0.166-25.657,0.338L108.76,5.897l-32.11-0.003l-0.006,52.728\
+                    c-6.959,0.142-13.793,0.277-20.466,0.277v-0.156l-44.33-0.018l0.006,34.282c0,0,23.734-0.446,23.343-0.013\
+                    c13.013,0.009,17.262,7.559,18.484,14.076l0.01,60.083v84.397c-0.573,4.09-2.984,10.625-12.083,10.637\
+                    c0.414,0.364-23.379-0.004-23.379-0.004l-6.375,38.335h41.817c7.792,0.009,15.448,0.13,22.959,0.19l0.028,53.338l32.102,0.009\
+                    l-0.009-52.779c8.832,0.18,17.357,0.258,25.684,0.247l-0.009,52.532h32.138l0.018-53.249c54.022-3.1,91.842-16.697,96.544-67.385\
+                    C266.916,192.612,247.692,174.396,217.021,167.042z M109.535,95.321c18.126,0,75.132-5.767,75.14,32.064\
+                    c-0.008,36.269-56.996,32.032-75.14,32.032V95.321z M109.521,262.447l0.014-70.672c21.778-0.006,90.085-6.261,90.094,35.32\
+                    C199.638,266.971,131.313,262.431,109.521,262.447z'/>\
+            </g>\
+        </g>\
+        </svg>";
+    
     var rootUrl = "http://localhost:3000",
         thisIssueUrl = encodeURI(window.location.href);
 
@@ -215,48 +234,61 @@
          */
         setupPostBounty: function (initValue) {
             ui.render(function () {
-                
-                var btcSymbol = "\
-                    <svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'\
-                         viewBox='0 0 273.6 360' enable-background='new 0 0 273.6 360' xml:space='preserve'\
-                         id='btcSymbol'>\
-                    <g>\
-                        <g>\
-                            <path fill='#333' d='M217.021,167.042c18.631-9.483,30.288-26.184,27.565-54.007c-3.667-38.023-36.526-50.773-78.006-54.404l-0.008-52.741\
-                                h-32.139l-0.009,51.354c-8.456,0-17.076,0.166-25.657,0.338L108.76,5.897l-32.11-0.003l-0.006,52.728\
-                                c-6.959,0.142-13.793,0.277-20.466,0.277v-0.156l-44.33-0.018l0.006,34.282c0,0,23.734-0.446,23.343-0.013\
-                                c13.013,0.009,17.262,7.559,18.484,14.076l0.01,60.083v84.397c-0.573,4.09-2.984,10.625-12.083,10.637\
-                                c0.414,0.364-23.379-0.004-23.379-0.004l-6.375,38.335h41.817c7.792,0.009,15.448,0.13,22.959,0.19l0.028,53.338l32.102,0.009\
-                                l-0.009-52.779c8.832,0.18,17.357,0.258,25.684,0.247l-0.009,52.532h32.138l0.018-53.249c54.022-3.1,91.842-16.697,96.544-67.385\
-                                C266.916,192.612,247.692,174.396,217.021,167.042z M109.535,95.321c18.126,0,75.132-5.767,75.14,32.064\
-                                c-0.008,36.269-56.996,32.032-75.14,32.032V95.321z M109.521,262.447l0.014-70.672c21.778-0.006,90.085-6.261,90.094,35.32\
-                                C199.638,266.971,131.313,262.431,109.521,262.447z'/>\
-                        </g>\
-                    </g>\
-                    </svg>";
-                
                 var currencyToggle = "<div id='currencyToggle' class='toggle-light'></div>";
                 $(currencyToggle).insertAfter(ui._container);
                 
-                var bountyDiv = "" +
-                    "<div class='inputWrapper'><label for='bountyInput' class='bountyCurrency'>" +
-                    "<span id='usdSymbol'>$</span>" +
-                    btcSymbol +
-                    "</label>" +
+                var usdDiv = "" +
+                    "<div class='usd inputWrapper'><label for='bountyInput' class='bountyCurrency currencySymbol'>$</label>" +
                     "<input id='bountyInput' type='number' value='" + initValue + "' min='0' step='5'/></div>" +
                     "<input id='currencyInput' type='hidden' value='usd' />" + 
                     "<a id='addBounty' class='bountyButton button minibutton bigger' href='#'>" +
                     "Post Bounty" +
                     "</a>";
 
-                $(bountyDiv).insertAfter(ui._container);
+                $(usdDiv).insertAfter(ui._container);
                 
+                // Generate a Bitcoin address for this issue/user pair.
+                // TODO: Move this inside the toggle code?
+                messenger.sendMessage(
+                    {
+                        method: "btcAddressForIssue",
+                        params: [thisIssueUrl]
+                    },
+                    
+                    function (message) {
+                        if (message.error)
+                            return;
+
+                        if (message.result) {
+                            var btcDiv = "" +
+                                "<div class='btc inputWrapper'>" +
+                                "<div class='qrCode' data='" + message.result + "'></div>" +
+                                "<div class='btcAddress'><a href='bitcoin:" + message.result + "'>" + message.result + "</a></div>" +
+                                "</div>";
+                            $(btcDiv).insertAfter(ui._container);
+                            
+                            $('.qrCode').each(function() {
+                                var $this = $(this);
+                                var size = Math.min(82, $this.parent().width());
+                                $this.qrcode({
+                                  width: size,
+                                  height: size,
+                                  text: $this.attr("data")
+                                });
+                                
+                                $('canvas', $this).wrap(
+                                    $('<a>').attr('href', 'bitcoin:' + $this.attr("data")));
+                            });
+                        }
+                    }
+                );
                 
                 $("#currencyToggle")
                 .on('toggle', function (e, usd) {
-                    $("#usdSymbol").toggle(usd);
-                    $("#btcSymbol").toggle(!usd);
+                    $(".usd.inputWrapper, #addBounty").toggle(usd);
+                    $(".btc.inputWrapper").toggle(!usd);
                     $("#currencyInput").val(usd ? "usd" : "btc");
+                    //$(".currencySymbol").html(usd ? "$" : btcSymbol);
                 })
                 .toggles({
                     animate: 0,
@@ -304,7 +336,7 @@
          */
         setBountyAmount: function (amount) {
             ui.render(function () {
-                $(".state-indicator.open").html("Open $" + amount);
+                $(".state-indicator.open").html("Open <span class='currencySymbol'>$</span>" + amount);
             }, "setBountyAmount");
         },
 
