8dcbf4266e186f9b9748f1026e34a3b1f75b2121
Ryan Casey <ryepdx@gmail.com>
2013-05-19 15:56:37 -0700

    Moved Bitcoin address assignment logic out of routes.js and into packages/bitcoin/server/bitcoin.js

--- meteor/packages/bitcoin/bitcoin.js
+++ meteor/packages/bitcoin/bitcoin.js
@@ -2,3 +2,33 @@
 
 Bitcoin = {};
 
+/***********************************************
+ * Returns a BitcoinAddress for the given issue/
+ * user pair. If one does not exist, it assigns
+ * one from the pool of unassigned addresses.
+ * @param userId
+ * @param url
+ * @return {BitcoinAddress}
+ **/
+Bitcoin.addressForIssue = function (userId, url) {
+    var address = BitcoinAddresses.findOne(
+    { url: url, userId: userId }, { reactive: false });
+     
+    // If there is no address associated with this user and issue,
+    // grab an unused one and associate it.
+    if (!address) {
+        address = BitcoinAddresses.findOne(
+           { used: false }, { reactive: false } );
+           
+        if (address) {
+            Fiber(function () {
+                BitcoinAddresses.update({ address: address.address },
+                    { $set: { used: true, url: url, userId: userId } });
+            }).run();
+        } else {
+            throw "no bitcoin addresses loaded!";
+        }
+    }
+    
+    return address;
+};
