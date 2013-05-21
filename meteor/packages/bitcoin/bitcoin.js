7672e5de1566f504eb571df0d42a5320a92fbbd3
Ryan Casey <ryepdx@gmail.com>
2013-05-21 03:01:45 -0700

    Payout code for Bitcoin written. Not yet tested, though.

--- meteor/packages/bitcoin/bitcoin.js
+++ meteor/packages/bitcoin/bitcoin.js
@@ -3,26 +3,26 @@
 Bitcoin = {};
 
 /***********************************************
- * Returns a BitcoinAddress for the given issue/
+ * Returns a Bitcoin.IssueAddress for the given issue/
  * user pair. If one does not exist, it assigns
  * one from the pool of unassigned addresses.
  * @param userId
  * @param url
- * @return {BitcoinAddress}
+ * @return {Bitcoin.IssueAddress}
  **/
 Bitcoin.addressForIssue = function (userId, url) {
-    var address = BitcoinAddresses.findOne(
+    var address = Bitcoin.IssueAddresses.findOne(
     { url: url, userId: userId }, { reactive: false });
      
     // If there is no address associated with this user and issue,
     // grab an unused one and associate it.
     if (!address) {
-        address = BitcoinAddresses.findOne(
+        address = Bitcoin.IssueAddresses.findOne(
            { used: false }, { reactive: false } );
            
         if (address) {
             Fiber(function () {
-                BitcoinAddresses.update({ address: address.address },
+                Bitcoin.IssueAddresses.update({ address: address.address },
                     { $set: { used: true, url: url, userId: userId } });
                     
                 // Set the address's "account" via bitcoind,
@@ -56,4 +56,67 @@ Bitcoin.verify = function (request, response, callback) {
     if (callback) {
         callback(error, params);
     }
+};
+
+/**
+ * Make a payment to a set of receivers
+ * @param address The address we're paying from.
+ * @param receiverList ex. [{email: "perl.jonathan@gmail.com", amount: 100.12}, ..]
+ * @param callback (error, data)
+ */
+Bitcoin.pay = function (address, receiverList, callback) {
+    // Only pay the people getting > 0 BTC
+    receiverList = _.filter(receiverList, function (receiver) {
+        return receiver.amount > 0;
+    });
+
+    // Make sure that we're not paying more than our receiving address received.
+    var totalPayout = _.reduce(receiverList, function (receiverA, receiverB) {
+        return {"amount": receiverA.amount + receiverB.amount};
+    }, {"amount": 0});
+    
+    // We're using getReceivedByAddress instead of getting the balance of the
+    // address because the address could technically be empty. We're only
+    // keeping a small portion of the total bounties in the hot wallet.
+    Bitcoin.Client.getReceivedByAddress(address, function (err, received) {        
+        
+        // After fees, total payout should be less than what this address received.
+        if (totalPayout.amount < received) {
+            _.each(receiverList, function (receiver) {
+                
+                // Look for a Bitcoin address for this recipient.
+                // If they don't have one yet, grant them a temporary one
+                // on our server. When they join and set a Bitcoin address,
+                // we'll check for the temporary address and send its
+                // contents to the address they set.
+                var payoutAddress = Bitcoin.ReceiverAddresses.find(
+                    {"email": receiver.email}, {"reactive": false});
+                    
+                if (payoutAddress) {
+                    // Send the amount owed the recipient.
+                    Bitcoin.Client.sendToAddress(payoutAddress, receiver.amount);
+                    
+                } else {
+                    // Grant the recipient an address in our hot wallet
+                    // and then send the amount owed to that address.
+                    Bitcoin.Client.getAccountAddress(receiver.email,
+                    function (err, payoutAddress) {
+                        Bitcoin.Client.sendToAddress(payoutAddress, receiver.amount);
+                    });
+                }
+            });
+            
+        // Okay, if that's not the case, we need to raise an alarm.
+        // We're doing this check because the application should never enter
+        // this state under its normal operating parameters. It's possible
+        // someone's trying to hack us, as this should be a fairly obvious
+        // attack vector.
+        } else {
+            // Only logging the error. It's a good idea to give out less
+            // information rather than more in cases like this.
+            console.log("ALERT: Payout greater than bitcoin received " +
+                "attempted from Bitcoin address " + address + ". Payout was " +
+                totalPayout.amount + " and total received was " + received);
+        }
+    });
 };
