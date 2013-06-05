f799ce9a08b59eff046f0884306b9cf5366afcd3
Jon Perl <perl.jonathan@gmail.com>
2013-06-04 22:36:59 -0400

    Moved deploy configurable settings to root settings folder

--- meteor/packages/bitcoin/server/settings.js
+++ meteor/packages/bitcoin/server/settings.js
@@ -1,5 +1,4 @@
-//TODO move all private info to root Meteor.settings settings.json?
-Bitcoin.IPNSecret = "moEcxlV1bvVaXIqvAQ1NwF5ZtdMJeyrET5rRQtaWi32Qez3y82nEvEssDLw2Qq5";
+Bitcoin.IPNSecret = Meteor.settings["BITCOIN_SECRET"];
 
 Bitcoin.Settings = {
     "callbackURI": Meteor.settings["ROOT_URL"] + "bitcoin-ipn?secret=" + Bitcoin.IPNSecret,
@@ -7,11 +6,6 @@ Bitcoin.Settings = {
     "maximumAddresses": 600,
     "addressRefillInterval": 1000, // In milliseconds.
     "maximumErrors": 10,
-    "client": {
-        "host": "localhost",
-        "port": 18332,
-        "username": "oldgregg",
-        "password": "4Mr7LjcTbx66DpVvzkA93AvJesx6HpSY4974CXn57TXZ"
-    },
+    "client": Meteor.settings["BITCOIN_CLIENT"],
     "minimumConfirmations": 6
 };
