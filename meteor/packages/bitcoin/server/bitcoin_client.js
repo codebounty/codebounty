054e7da752984cafd11bdbed3a234aa0f8c039c7
Ryan Casey <ryepdx@gmail.com>
2013-06-07 15:50:55 -0700

    Got rid of bitcoin.Client deprecation warning.

--- meteor/packages/bitcoin/server/bitcoin_client.js
+++ meteor/packages/bitcoin/server/bitcoin_client.js
@@ -1,6 +1,3 @@
 var bitcoin = Npm.require("bitcoin");
 
-Bitcoin.Client = new bitcoin.Client(
-    Bitcoin.Settings.client.host, Bitcoin.Settings.client.port,
-    Bitcoin.Settings.client.username, Bitcoin.Settings.client.password
-);
+Bitcoin.Client = new bitcoin.Client(Bitcoin.Settings.client);
