91adf778cca350f0d468c27434ae09e0bc5944b6
Kaiwen Xu <kevin@kevxu.net>
2013-06-13 21:13:39 -0400

    Adding new grunt tasks.
    Added grunt build for build chrome extensions.
    Added grunt clean for cleaning dist folder.
    Added grunt server for running all servers (including meteor).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,29 @@
 module.exports = function (grunt) {
+    var gruntConfig = {
+        dist: "build"
+    };
 
     grunt.initConfig({
+        config: gruntConfig,
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        "<%= config.dist %>/*",
+                        "!<%= config.dist %>/.git*"
+                    ]
+                }]
+            }
+        },
+        crx: {
+            dist: {
+                src: "extensions/chrome/",
+                dest: "<%= config.dist %>/codebounty.crx",
+                exclude: [".git", "*.pem"],
+                privateKey: "extensions/chrome.pem"
+            }
+        },
         intern: {
             client: {
                 options: {
@@ -9,8 +32,37 @@ module.exports = function (grunt) {
                     runType: "runner"
                 }
             }
+        },
+        bgShell: {
+            bitcoind: {
+                cmd: "bitcoind",
+                bg: true,
+                stdout: false,
+                stderr: false
+            },
+            meteor: {
+                cmd: [
+                    "cd meteor",
+                    "mrt --settings settings.json"
+                ].join("&&"),
+                bg: false,
+                stdout: true,
+                stderr: true
+            },
+            webserver: {
+                cmd: [
+                    "cd tools",
+                    "node web-server.js"
+                ].join("&&"),
+                bg: true,
+                stdout: false,
+                stderr: false
+            }
         }
     });
+
+    // Load all grunt tasks
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
     // Loading using a local git copy
     grunt.loadNpmTasks("intern");
@@ -18,6 +70,18 @@ module.exports = function (grunt) {
     // Register a test task
     grunt.registerTask("test", ["intern:client"]);
 
+    // Register a build task
+    grunt.registerTask("build", [
+        "clean:dist",
+        "crx:dist"
+    ]);
+
+    grunt.registerTask("server", [
+        "bgShell:webserver",
+        "bgShell:bitcoind",
+        "bgShell:meteor"
+    ]);
+
     // By default we just test
-    grunt.registerTask("default", ["test"]);
+    grunt.registerTask("default", ["build"]);
 };
