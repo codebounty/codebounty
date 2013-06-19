16732df99f9ce25d774c65d7dfbffbbf3e13e4ef
Jon Perl <perl.jonathan@gmail.com>
2013-06-19 00:14:36 -0400

    Moved chrome extension content inside extension

--- extensions/chrome/chromeContent.js
+++ extensions/chrome/chromeContent.js
@@ -1,30 +1,28 @@
-//Inject css - will probably want to squash all into one file someday.
+var contentUrl = function (file) {
+    return chrome.extension.getURL("content/" + file);
+};
 
-//TODO uncomment for deployment
-// var publicUrl = "https://app.codebounty.co/";
-var publicUrl = "https://localhost:8888/meteor/public/"; //locally we use the node server because it is https
-
-var css = [
-    publicUrl + "codebounty.css",
-    publicUrl + "toggles.css",
-    publicUrl + "toggles-github.css"
+var cssFiles = [
+    "codebounty.css",
+    "toggles.css",
+    "toggles-github.css"
 ];
-var link;
 
+var link;
 for (var i = 0; i < css.length; i++) {
     link = document.createElement("link");
-    link.href = css[i];
+    link.href = contentUrl(cssFiles[i]);
     link.type = "text/css";
     link.rel = "stylesheet";
     document.getElementsByTagName("head")[0].appendChild(link);
 }
 
 //Inject javascript - will probably want to squash all into one file someday.
-var js = [
-    publicUrl + "github.js",
-    publicUrl + "toggles.min.js"
+var jsFiles = [
+    "github.js",
+    "toggles.min.js"
 ];
 
 for (var i = 0; i < js.length; i++) {
-    document.body.appendChild(document.createElement("script")).src = js[i];
+    document.body.appendChild(document.createElement("script")).src = contentUrl(jsFiles[i]);
 }
