62c1ed7cf97e6a8a1bf9e5294e8b28cf94441244
Jon Perl <perl.jonathan@gmail.com>
2013-06-29 23:05:51 -0400

    No longer logging http because nginx will do that

--- meteor/packages/observatory-settings/ObservatorySettings.js
+++ meteor/packages/observatory-settings/ObservatorySettings.js
@@ -11,6 +11,8 @@ ObservatorySettings = {
             return false;
         }
     },
+    log_http: false,
+    printToConsole: false,
     should_publish: function (publish) {
         //only publish logs to the admins
         return Meteor.users.findOne({ _id: publish.userId, role: "admin"});
