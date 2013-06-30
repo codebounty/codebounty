47d11f87cfa67a0db18c98649bec9a2d1323bada
Ryan Casey <ryepdx@gmail.com>
2013-06-30 14:02:20 -0700

    Began implementing the PageObject design pattern: http://docs.seleniumhq.org/docs/06_test_design_considerations.jsp#page-object-design-pattern

--- tests/lib/bounty.js
+++ tests/lib/bounty.js
@@ -1,29 +1,61 @@
+function LoginPage (browser, username, password) {
+    this.rootUrl = "https://github.com/login";
+    this.browser = browser;
+    this.username = username;
+    this.password = password;
+    return this;
+}
+
+LoginPage.prototype.login = function () {
+    this.browser.get(this.rootUrl)
+        .elementByName("login").clickElement().type(this.username)
+        .end()
+        .elementByName("password").clickElement().type(this.password)
+        .end()
+        .elementByName("commit").clickElement()
+        .end()
+        .wait(1000);
+        
+    return this;
+}
+
+function IssuePage(browser, issue) {
+    this.rootUrl = "https://github.com/codebounty/codebounty/issues/";
+    this.browser = browser;
+    this.issueUrl = this.rootUrl + issue;
+    return this;
+}
+
+IssuePage.prototype.load = function () {
+    this.browser.get(this.issueUrl).wait(5000);
+    return this;
+}
+
+IssuePage.prototype.placeBounty = function (currency, amount) {
+    return this;
+}
+
 define([
     "intern!object",
     "intern/chai!assert",
     "../settings"
 ], function (registerSuite, assert, settings) {
-    var gitHub = "https://github.com/";
-    var issueUrl = gitHub + "codebounty/codebounty/issues/26";
-    var loginUrl = gitHub + "login";
+    var testIssue = "26";
 
     registerSuite({
         name: "bounty",
 
         "post bounty": function () {
             var browser = this.remote;
-            return browser.get(loginUrl)
-                .elementByName("login").clickElement().type(settings.GITHUB_USERNAME)
-                .end()
-                .elementByName("password").clickElement().type(settings.GITHUB_PASSWORD)
-                .end()
-                .elementByName("commit").clickElement()
-                .end()
-                .wait(1000)
-                .get(issueUrl)
-                .wait(5000);
-
-            //TODO
+            
+            // Log in as our test user.
+            (new LoginPage(
+                browser, settings.GITHUB_USERNAME, settings.GITHUB_PASSWORD))
+            .login();
+            
+            // Load the issue page and place a bounty.
+            return (new IssuePage(browser, testIssue))
+                .load();
         }
     });
 });
