3f334069d1c3042cc7539da577caf373f6e37785
Jon Perl <perl.jonathan@gmail.com>
2013-06-29 23:05:51 -0400

    No longer logging http because nginx will do that

--- meteor/packages/logger/logger.js
+++ meteor/packages/logger/logger.js
@@ -1,6 +1,6 @@
 TL = TLog.getLogger();
 
-//all the different modules errors can come from
+//all the different modules log events can come from
 Modules = {
     Bitcoin: "Bitcoin",
     Bounty: "Bounty",
