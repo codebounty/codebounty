0983304a3ce3dd758a613efe97bdfde013e0635a
Jon Perl <perl.jonathan@gmail.com>
2013-07-20 16:52:27 -0400

    Issue #60 Cleaned up directory structure for tests. Setup testing for the comment after a bounty is posted

--- features/post_bounty.feature
+++ features/post_bounty.feature
@@ -5,4 +5,4 @@ Feature: Backer posts a bounty
   Scenario: Backer posts a USD bounty
     Given I visit issue 25 in codebounty/codebounty
     When I post a 5 USD bounty
-    Then a bounty comment should be posted in issue 25
+    Then a bounty comment should be posted on the issue
--- features/step_definitions/bounty.js
+++ features/step_definitions/bounty.js
@@ -1,125 +1,18 @@
 module.exports = function () {
-    var browser, settings, webdriver;
-
     this.World = require("../support/world.js").World;
 
-    function IssuePage(number, organization, repo, callback) {
-        var self = this;
-
-        self.issueUrl = "https://github.com/" + organization + "/" + repo + "/issues/" + number;
-
-        //store current window handle, switch windows, login with paypal, approve payment
-        browser.getAllWindowHandles().then(function (handles) {
-            self.handle = handles[0];
-        });
-
-        self._navigate().then(function () {
-            //it could take up to 5 seconds after we navigated for the extension to load
-            //todo improve performance
-            browser.manage().timeouts().implicitlyWait(5000);
-
-            callback();
-        });
-    }
-
-    IssuePage.prototype._login = function () {
-        browser.findElement({name: "login"}).sendKeys(settings.GITHUB_USERNAME);
-        browser.findElement({name: "password"}).sendKeys(settings.GITHUB_PASSWORD);
-        browser.findElement({name: "commit"}).click();
-
-        return browser.sleep(1000);
-    };
-
-    IssuePage.prototype._navigate = function () {
-        var self = this;
-
-        browser.get(self.issueUrl);
-        browser.sleep(1000);
-
-        return browser.isElementPresent({name: "login"}).then(function (shouldLogin) {
-            if (!shouldLogin)
-                return;
-
-            return self._login();
-        });
-    };
-
-    IssuePage.prototype.toggleCurrency = function () {
-        return browser.findElement({id: "currencyToggle"})
-            .findElement({className: "toggle-select"})
-            .click();
-    };
-
-    /**
-     * Returns the approval page handle
-     */
-    IssuePage.prototype.postBounty = function () {
-        var self = this;
-
-        browser.findElement({id: "postBounty"}).click();
-
-        return browser.getAllWindowHandles().then(function (handles) {
-            for (var i = 0; i < handles.length; i++)
-                //there should only be two windows open
-                //the one that is not this issue page should is the approval page
-                if (handles[i] !== self.handle)
-                    return handles[i];
-        });
-    };
+    var IssuePage = require("../page_objects/issue_page.js")(),
+        PaypalApprovalPage = require("../page_objects/paypal_approval_page.js")();
 
     var currentIssuePage;
 
     this.Given(/^I visit issue (\d+) in (.*)\/(.*)$/, function (number, organization, repo, callback) {
-        browser = this.browser;
-        settings = this.settings;
-        webdriver = this.webdriver;
-
-        currentIssuePage = new IssuePage(number, organization, repo, callback);
+        currentIssuePage = new IssuePage(this, number, organization, repo, callback);
     });
 
-    function PaypalApprovalPage(handle) {
-        this.handle = handle;
-
-        browser.switchTo().window(handle);
-
-        this._login();
-    }
-
-    PaypalApprovalPage.prototype._login = function () {
-        //sometime the login page vs credit card page shows up
-        //so click the login button to get to the login page
-        browser.isElementPresent({id: "login_button"}).then(function (loginButton) {
-            if (loginButton)
-                return browser.findElement({id: "login_button"}).click();
-        });
-
-        var loginEmail = browser.findElement({id: "login_email"});
-        loginEmail.clear();
-        loginEmail.sendKeys(settings.PAYPAL_USERNAME);
-
-        browser.findElement({id: "login_password"})
-            .sendKeys(settings.PAYPAL_PASSWORD);
-
-        browser.findElement({id: "submitLogin"}).click();
-
-        return browser.sleep(4000);
-    };
-
-    PaypalApprovalPage.prototype.approve = function () {
-        browser.findElement({id: "submit.x"}).click();
-
-        //it takes some time for paypal to process the approval after clicking
-        return browser.sleep(3000);
-    };
-
-    function BitcoinApprovalPage(handle) {
-        this.handle = handle;
-        browser.switchTo().window(handle);
-
-        //TODO if the user has not set a return address, do so here
-    }
-
     this.When(/I post a (\d+) (.*) bounty/, function (amount, currency, callback) {
+        var self = this;
+
         if (currency === "BTC") {
             currentIssuePage.toggleCurrency();
             throw "Not implemented yet";
@@ -127,11 +20,25 @@ module.exports = function () {
 
         currentIssuePage.postBounty().then(function (approvalHandle) {
             if (currency === "USD") {
-                var paypalApprovalPage = new PaypalApprovalPage(approvalHandle);
+                self.browser.switchTo().window(approvalHandle);
 
-                paypalApprovalPage.approve()
+                var paypalApprovalPage = new PaypalApprovalPage(self, approvalHandle);
+
+                paypalApprovalPage
+                    .approve()
                     .then(callback);
             }
+        });
+    });
+
+    this.Then(/a bounty comment should be posted on the issue/, function (callback) {
+        this.browser.switchTo().window(currentIssuePage.handle);
+
+        currentIssuePage.isBountyCommentPresent().then(function (isPresent) {
+            if (isPresent)
+                callback();
+            else
+                callback.fail();
         });
     });
 };
