9ac10000dd5b778613cd341bffa6e6faf0e4d9af
Jon Perl <perl.jonathan@gmail.com>
2013-07-21 16:49:51 -0400

    Improved reward screen styling for other sizes. Updated dev settings config to proper app. Updated server to always reset account login configuration (at least for development).

--- meteor/server/server.js
+++ meteor/server/server.js
@@ -1,15 +1,11 @@
 Meteor.startup(function () {
-    var githubServiceSetup = Accounts.loginServiceConfiguration.find({service: "github"}).count() === 1;
-    if (!githubServiceSetup) {
-        //setup authentication Provider
-        Accounts.loginServiceConfiguration.remove({
-            service: "github"
-        });
+    Accounts.loginServiceConfiguration.remove({
+        service: "github"
+    });
 
-        Accounts.loginServiceConfiguration.insert({
-            service: "github",
-            clientId: Meteor.settings["GITHUB_CLIENTID"],
-            secret: Meteor.settings["GITHUB_SECRET"]
-        });
-    }
+    Accounts.loginServiceConfiguration.insert({
+        service: "github",
+        clientId: Meteor.settings["GITHUB_CLIENTID"],
+        secret: Meteor.settings["GITHUB_SECRET"]
+    });
 });
