77f1187b15354ab7dd1d3be4d16b8dfa39d57b5f
Jon Perl <perl.jonathan@gmail.com>
2013-07-22 22:21:49 -0400

    Setup selenium to check and split up reward equally. Need to fixup reward screen though.

--- features/step_definitions/reward.js
+++ features/step_definitions/reward.js
@@ -1,6 +1,8 @@
 module.exports = function () {
     this.World = require("../support/world.js").World;
 
+    var Big = require("big.js");
+
     var IssuePage = require("../page_objects/issue_page.js").IssuePage,
         RewardPage = require("../page_objects/reward_page.js").RewardPage;
 
@@ -20,7 +22,7 @@ module.exports = function () {
         currentIssuePage.rewardBounty();
 
         //give time for the reward to load
-        self.browser.sleep(3000);
+        self.browser.sleep(3500);
 
         currentRewardPage = new RewardPage(self);
         currentRewardPage.switchTo();
@@ -28,23 +30,35 @@ module.exports = function () {
         //equally split the amount among each contributor
         currentRewardPage.amount().then(function (amount) {
             currentRewardPage.contributorRows().then(function (contributorRows) {
-                var promises = [];
+                var promises = [],
+                    numberOfContributors = contributorRows.length;
 
-                console.log(amount, "/", contributorRows.length, promises.length);
+                amount = new Big(amount);
+                var equalAmount = new Big(amount.div(numberOfContributors).toFixed(2));
+                var remainder = amount.minus(equalAmount.times(numberOfContributors));
 
                 //check the contributors before setting the amounts
                 //because every time a contributor is set the amounts reset
                 //TODO fix this?
-                for (var i = 0; i < contributorRows.length; i++)
+                for (var i = 0; i < numberOfContributors; i++)
                     promises.push(currentRewardPage.checkContributor(i));
 
-                //wait a half second for the browser to re-render
+                //wait a second for the browser to re-render
                 //TODO fix this?
-                self.browser.sleep(500);
+                self.browser.sleep(1000);
 
-                for (i = 0; i < contributorRows.length; i++)
-                    promises.push(currentRewardPage.setContributorAmount(i, 13));
+                for (i = 0; i < numberOfContributors; i++) {
+                    var contributorReward = equalAmount;
 
+                    //add any remainder to the first contributor
+                    if (i === 0)
+                        contributorReward = equalAmount.plus(remainder);
+
+                    console.log(contributorReward.toString());
+
+                    contributorReward = contributorReward.toFixed(2);
+                    promises.push(currentRewardPage.setContributorAmount(i, contributorReward));
+                }
 
                 return self.webdriver.promise.fullyResolved(promises);
             });
