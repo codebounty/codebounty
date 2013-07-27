3f2b6c16d3805c7fdd1f48f18d1e4f2291b03e1f
Ryan Casey <ryepdx@gmail.com>
2013-07-26 18:02:14 -0700

    Enabled the use of encrypted wallets for the sendToAddress command. Need to make a more generalized scheme (a proper Decorator instead of the ad-hoc one we're using now) in the future, but this one does work for now.

--- meteor/packages/bitcoin/server/bitcoin_client.js
+++ meteor/packages/bitcoin/server/bitcoin_client.js
@@ -1,3 +1,33 @@
 var bitcoin = Npm.require("bitcoin");
+var ERR_CODE_WALLET_LOCKED = -13;
 
-Bitcoin.Client = new bitcoin.Client(Bitcoin.Settings.client);
+// A wrapper for commands that require an unlocked wallet.
+var BitcoinClient = bitcoin.Client;
+BitcoinClient.prototype.withUnlockedWallet = function (command, callback) {
+    var that = this;
+    // Try once, and if we get a failure due to an encrypted wallet,
+    // we'll try again after unlocking the wallet.
+    command(function () {
+        if (arguments[0] && arguments[0].code == ERR_CODE_WALLET_LOCKED) {
+            // Unlock the wallet and try again.
+            that.walletPassphrase(
+                Bitcoin.Settings.walletPassphrase, 
+                Bitcoin.Settings.walletLockInterval,
+                (function () { command(callback); }));
+        } else if (callback) {
+            // If the call succeeded, pass on the results to its callback.
+            callback.apply(callback, arguments);
+        }
+    });
+};
+
+// Override the default sendToAddress function
+BitcoinClient.prototype._sendToAddress  = BitcoinClient.prototype.sendToAddress;
+BitcoinClient.prototype.sendToAddress = function(address, amount, callback) {
+    var that = this;
+    this.withUnlockedWallet(function (_callback) {
+       that._sendToAddress(address, amount, _callback);
+    }, callback);
+}
+
+Bitcoin.Client = new BitcoinClient(Bitcoin.Settings.client);
