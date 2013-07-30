6fca1e9cb08dec1548e599a45395fcf15bdb2a59
Ryan Casey <ryepdx@gmail.com>
2013-07-30 16:42:04 -0700

    Simplified the 'withUnlockedWallet' decorator.

--- meteor/packages/bitcoin/server/bitcoin_client.js
+++ meteor/packages/bitcoin/server/bitcoin_client.js
@@ -1,60 +1,85 @@
 var bitcoin = Npm.require("bitcoin");
 var ERR_CODE_WALLET_LOCKED = -13;
 
+/**
+ * Takes a Bitcoin client object, a zero-parameter function encapsulating the
+ * original request to the Bitcoin client, and a callback function. Returns a
+ * function that can be passed to a Bitcoin client object as a callback. If the
+ * request fails due to the wallet being locked, the callback function returned
+ * by this function will retry the original request after unlocking the wallet.
+ * If the request is successful either time, it will forward the response
+ * transparently to the callback function passed in.
+ * 
+ * @param client A Bitcoin client object
+ * @param originalRequest A zero-parameter function encapsulating the request.
+ * @param callback A function that receives the results of the client request.
+ * @return function A callback function that can be passed to the Bitcoin client.
+ **/
+Bitcoin._encryptedWalletCallbackDecorator = function (client, originalRequest, callback) {
+    return function () {
+        if (arguments[0] && arguments[0].code == ERR_CODE_WALLET_LOCKED) {
+            // Unlock the wallet and try again.
+            client.walletPassphrase(
+                Meteor.settings["BITCOIN_PASSPHRASE"], 
+                Meteor.settings["BITCOIN_LOCK_INTERVAL"],
+                originalRequest);
+        } else if (callback) {
+            // If the call succeeded, pass on the results to its callback.
+            callback.apply(callback, arguments);
+        }
+    };
+}
+
+/**
+ * Examines an arguments array/object to see if the last argument is a function.
+ * If it is, it returns it. Otherwise it returns undefined.
+ * @param args An arguments array or object, as supplied by Javascript.
+ * @return function
+ **/
+Bitcoin.getCallbackFromArgs = function (args) {
+    var tail = args[args.length-1];
+    return (_.isFunction(tail) ? tail : undefined);
+};
+
+/**
+ * Either tacks the passed-in callback function onto the end of the passed-in
+ * arguments object, or overwrites the last argument in the arguments object
+ * if that argument is a function.
+ * 
+ * @param args The arguments object to change.
+ * @param callback The callback to overwrite the callback function in args with.
+ * @return args
+ **/
+Bitcoin.overrideCallbackInArgs = function (args, callback) {
+    if (_.isFunction(args[args.length-1])) {
+        args[args.length-1] = callback;
+    } else {
+        // args is an object masquerading as an array,
+        // so we have to increment its length property
+        // if we're adding a callback.
+        args[args.length] = callback;
+        args.length++;
+    }
+    return args;
+}
+
 // A decorator for bitcoin.Client commands that require an unlocked wallet.
 // Calling a bitcoin.Client command that has been wrapped in this decorator
 // results in the command being called once and then re-called after unlocking
 // the wallet if the command fails due to the wallet being locked.
 Bitcoin.withUnlockedWallet = function (command) {
+    
     return function () {
         var that = this;
-        var args = arguments;
-        var tail = args[args.length-1];
-        var callback = (_.isFunction(tail) ? tail : undefined);
-        
-        
-        // Takes a function that takes a modified callback and passes it
-        // on to a Bitcoin client command. The modified callback will
-        // retry the original command with the original callback after
-        // unlocking the wallet with the wallet passphrase defined in the
-        // settings file.
-        // I'm defining this function here because it doesn't make any 
-        // sense to expose it. Would be awesome if we could refactor all
-        // this to make it simpler sometime, but this works for now.
-        var retryIfLocked = function (command) {
-            // Try once, and if we get a failure due to an encrypted wallet,
-            // we'll try again after unlocking the wallet.
-            command(function () {
-                if (arguments[0] && arguments[0].code == ERR_CODE_WALLET_LOCKED) {
-                    // Unlock the wallet and try again.
-                    that.walletPassphrase(
-                        Meteor.settings["BITCOIN_PASSPHRASE"], 
-                        Meteor.settings["BITCOIN_LOCK_INTERVAL"],
-                        (function () { command(callback); }));
-                } else if (callback) {
-                    // If the call succeeded, pass on the results to its callback.
-                    callback.apply(callback, arguments);
-                }
-            });
-        };
-        
-        // Here we are just creating a function that overrides whatever the
-        // original callback was (if there was one) with the modified callback
-        // from retryIfLocked. If there was no callback originally defined,
-        // it just adds the modified callback onto the end of the arguments
-        // to the original command.
-        retryIfLocked(function (_callback) {
-            if (_.isFunction(args[args.length-1])) {
-                args[args.length-1] = _callback;
-            } else {
-                // args is an object masquerading as an array,
-                // so we have to increment its length property
-                // if we're adding a callback.
-                args[args.length] = _callback;
-                args.length++;
-            }
-            command.apply(that, args);
+        var callback = Bitcoin.getCallbackFromArgs(arguments);
+        var originalArgs = arguments;
+        var originalRequest = (function () {
+            command.apply(that, originalArgs);
         });
+        var args = Bitcoin.overrideCallbackInArgs(arguments,
+            Bitcoin._encryptedWalletCallbackDecorator(that, originalRequest, callback));
+        
+        command.apply(that, args);
     };
 };
 
