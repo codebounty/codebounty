1d8788aeec78fcc9db220bb97e9362416b1627c6
Ryan Casey <ryepdx@gmail.com>
2013-08-16 18:26:43 -0700

    Meteor started complaining about a circular dependency, so I fixed it. Though that was after I updated...

--- meteor/packages/bitcoin/package.js
+++ meteor/packages/bitcoin/package.js
@@ -7,8 +7,6 @@ Npm.depends({
 });
 
 Package.on_use(function (api) {
-	api.use("bitcoin", "server");
-	
 	api.add_files("bitcoin.js", "server");
 	api.add_files("server/constants.js", "server");
     api.add_files("server/bitcoin_client.js", "server");
